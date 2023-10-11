3e12880c593a61e50b1c2a00982acb01b34b4465
ibbya42 <ibbyali4214@gmail.com>
2023-10-11 21:31:53 +1300

    11/10 Question 4 Push

--- Question 1-10/app.js
+++ Question 1-10/app.js
@@ -15,11 +15,40 @@
 // Question 2
 // Array Length 
 
-function arrLength(Length){
-    // length is used to calculate the amount of entriees in the array 
-    return Length.length
-}
+// function arrLength(Length){
+//     // length is used to calculate the amount of entriees in the array 
+//     return Length.length
+// }
 
-console.log(arrLength([1,2,3]))
-console.log(arrLength([5,0,4,1]))
-console.log(arrLength([]))
+// console.log(arrLength([1,2,3]))
+// console.log(arrLength([5,0,4,1]))
+// console.log(arrLength([]))
+
+// Question 3
+// Get Last element of Array 
+
+// function lastElem (arr) {
+//     // Need to minus the length by 1 as the index starts from 0
+//     return arr[arr.length -1]
+// }
+
+// console.log(lastElem([3,2,0,6,2]))
+// console.log(lastElem(["Dog" , "Cat" , "Sheep"]))
+
+// Question 4
+// Sum of Array 
+
+// function arrSum(arr){
+//     // Create a variable to hold the sum 
+//     let sum = 0;
+//     // Loop through every element in the array 
+//     for ( let i = 0; i < arr.length ; i++){
+//         // The sum will equal to the the array values
+//         sum = sum + arr[i]
+//     }
+//     // Return Sum after all the elements have been looped through
+//     return sum;
+// }
+
+// console.log(arrSum([2,2,2]))
+// console.log(arrSum([100,200,500]))
